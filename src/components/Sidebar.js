624630a79ea9c3c5165f326523fcaa081948651d
Jonathan Park <johanpk10@gmail.com>
2021-11-26 19:16:55 -0800

    Edit sidebar menu items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,6 +15,13 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import MailTwoToneIcon from '@mui/icons-material/MailTwoTone';
+import NotificationsTwoToneIcon from '@mui/icons-material/NotificationsTwoTone';
+import AccountTreeOutlinedIcon from '@mui/icons-material/AccountTreeOutlined';
+import FormatListNumberedOutlinedIcon from '@mui/icons-material/FormatListNumberedOutlined';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import '../styles/Sidebar.css';
@@ -23,25 +30,54 @@ function Sidebar() {
     return (
         <nav className='sidebar-container'>
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
+                <ListItem button key='Home'>
                     <ListItemIcon>
-                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        <HomeOutlinedIcon />
                     </ListItemIcon>
-                    <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
+                    <ListItemText primary='Home' />
+                </ListItem>
+                <ListItem button key='Projects'>
                     <ListItemIcon>
-                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        <AccountTreeOutlinedIcon />
                     </ListItemIcon>
-                    <ListItemText primary={text} />
-                    </ListItem>
-                ))}
+                    <ListItemText primary='Projects' />
+                </ListItem>
+                <ListItem button key='Tasks'>
+                    <ListItemIcon>
+                        <FormatListNumberedOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='Tasks' />
+                </ListItem>
+                <ListItem button key='Team'>
+                    <ListItemIcon>
+                        <PeopleAltOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='Team' />
+                </ListItem>
+
+                <Divider sx={{ margin: 1 }}/>
+
+                <ListItem button key='Inbox'>
+                    <ListItemIcon>
+                        <MailTwoToneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='Inbox' />
+                </ListItem>
+                <ListItem button key='Notifications'>
+                    <ListItemIcon>
+                        <NotificationsTwoToneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='Notifications' />
+                </ListItem>
+
+                <Divider sx={{ margin: 1 }}/>
+
+                <ListItem button key='My Account'>
+                    <ListItemIcon>
+                        <SettingsApplicationsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='My Account' />
+                </ListItem>
             </List>
         </nav>
     );
