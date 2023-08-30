e20df2d4cbd39b6e94ed74241ad1a12221c02792
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-08-30 15:22:00 -0700

    Replace var with const

--- server/app.js
+++ server/app.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 //Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = process.env.MONGODB_URI;
+const mongoose = require('mongoose');
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB)
   .catch((err) => console.error(err));
 mongoose.connection.on('error', (err) => console.error(err));
