5a6c0962fe9013fef9a18868983e600c02bbbc52
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-09-11 12:01:27 -0700

    Add placeholder functions for projectController and taskController

---
+++ server/controllers/projectController.js
@@ -0,0 +1,32 @@
+const Project = require('../models/project');
+const { body, validationResult } = require('express-validator');
+
+exports.getAll = [
+    function (req, res, next) {
+        res.send('Project getAll not yet implemented');
+    }
+];
+
+exports.getById = [
+    function (req, res, next) {
+        res.send('Project getById not yet implemented');
+    }
+];
+
+exports.create = [
+    function (req, res, next) {
+        res.send('Project create not yet implemented');
+    }
+];
+
+exports.update = [
+    function (req, res, next) {
+        res.send('Project update not yet implemented');
+    }
+];
+
+exports.delete = [
+    function (req, res, next) {
+        res.send('Project delete not yet implemented');
+    }
+];
---
+++ server/controllers/taskController.js
@@ -0,0 +1,32 @@
+const Task = require('../models/task');
+const { body, validationResult } = require('express-validator');
+
+exports.getAll = [
+    function (req, res, next) {
+        res.send('Task getAll not yet implemented');
+    }
+];
+
+exports.getById = [
+    function (req, res, next) {
+        res.send('Task getById not yet implemented');
+    }
+];
+
+exports.create = [
+    function (req, res, next) {
+        res.send('Task create not yet implemented');
+    }
+];
+
+exports.update = [
+    function (req, res, next) {
+        res.send('Task update not yet implemented');
+    }
+];
+
+exports.delete = [
+    function (req, res, next) {
+        res.send('Task delete not yet implemented');
+    }
+];
