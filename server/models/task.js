f612acfa02488dc2c13c33ae156b00ddf4fd5652
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-09-13 13:41:34 -0700

    Fix typo

--- server/models/task.js
+++ server/models/task.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const TaskSchema = new Schema({
     title: { type: String, required: true, maxLength: 100 },
     description: { type: String, required: true },
-    project: { type: Schema.Types.ObjectId, ref: 'Project', requried: true },
+    project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
     dateCreated: { type: Date, required: true },
     createdBy: { type: Schema.Types.ObjectId, ref: 'Member', required: true },
     status: { type: String, required: true },
