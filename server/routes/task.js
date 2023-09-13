13b203a3acbc0ab174c9112d9da5409868bd8f3c
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-09-13 14:52:35 -0700

    Set mergeParams to true so that taskRouter can access req.params.postId

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const taskRouter = express.Router();
+const taskRouter = express.Router({ mergeParams: true });
 const taskController = require('../controllers/taskController');
 
 taskRouter.get('/', taskController.getAll);
