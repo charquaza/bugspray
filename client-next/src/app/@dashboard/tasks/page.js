20a82059461275c7f5745cdeede8fc7bda04fa61
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-10-20 20:24:15 -0700

    Fetch task data from backend api

--- client-next/src/app/@dashboard/tasks/page.js
+++ client-next/src/app/@dashboard/tasks/page.js
@@ -1,8 +1,78 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { apiURL } from '../../../../config.js';
+
 export default function TasksPage() {
+    const [taskList, setTaskList] = useState([]);
+
+    useEffect(() => {
+        async function getTaskList() {
+            try {
+                const fetchOptions = {
+                    method: 'GET',
+                    mode:'cors',
+                    credentials: 'include',
+                    cache: 'no-store'
+                };
+                const fetchURL = apiURL + '/projects/dummy-project-id/tasks';
+
+                const res = await fetch(fetchURL, fetchOptions);
+                const data = await res.json();
+                const taskListData = data.data;
+
+                setTaskList(taskListData);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        getTaskList();
+    }, []);
+
     return (
         <main>
             <h1>Tasks</h1>
             <p>You are logged in. This is the tasks page.</p>
+
+            <ol>
+                {
+                    taskList.map((task) => {
+                        return (
+                            <li key={task._id}>
+                                <ul>
+                                    <li>Title: {task.title}</li>
+                                    <li>Description: {task.description}</li>
+                                    <li>Project: {task.project.name}</li>
+                                    <li>Date Created: {task.dateCreated}</li>
+                                    <li>Created By: {
+                                        task.createdBy.firstName + ' ' + 
+                                        task.createdBy.lastName
+                                        }
+                                    </li>
+                                    <li>Status: {task.status}</li>
+                                    <li>Priority: {task.priority}</li>
+                                    <li>
+                                        Assignees: 
+                                        <ul>
+                                            {
+                                                task.assignees.map((member) => {
+                                                    return (
+                                                        <li key={member._id}>
+                                                            {member.firstName + ' ' + member.lastName}
+                                                        </li>
+                                                    );
+                                                })
+                                            }
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        );
+                    })
+                }
+            </ol>
+
         </main>
     );
 }
