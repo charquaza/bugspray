4f7057bfaa05b30c0bc15da1ef2e9aa3abee381a
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-10-25 15:30:34 -0700

    Implement sign up form

--- client-next/src/app/@landing/sign-up/page.js
+++ client-next/src/app/@landing/sign-up/page.js
@@ -1,8 +1,143 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { apiURL } from '../../../../config.js';
+
 export default function SignUpPage() {
+    const [inputValues, setInputValues] = useState({ 
+        firstName: '',
+        lastName: '',
+        role: '',
+        username: '', 
+        password: '',
+        confirmPassword: '' 
+    });
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [formErrors, setFormErrors] = useState([]);
+    
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!formSubmitted) {
+            return;
+        }
+
+        async function sendFormData() {
+            try {
+                var fetchOptions = {
+                    method: 'POST',
+                    headers: { 
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(inputValues),
+                    mode: 'cors',
+                    credentials: 'include',
+                    cache: 'no-store'
+                }
+                var fetchURL = apiURL + '/members/sign-up';
+        
+                var res = await fetch(fetchURL, fetchOptions);
+                var data = await res.json();
+        
+                if (res.ok) {
+                    router.push('/');
+                    router.refresh();
+                } else {
+                    setFormErrors(data.errors);
+                }
+            } catch (err) {
+                console.error('Sign up failed: ' + err);
+                setFormErrors('Network error: please try again later');
+            }
+
+            setFormSubmitted(false);
+        }
+    
+        sendFormData();
+    }, [formSubmitted]);
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        setFormSubmitted(true);
+    }
+
+    function handleInputChange(e) {  
+        var inputElem = e.target;
+        setInputValues(prevState => {
+            return { ...prevState, [inputElem.name]: inputElem.value };
+        });      
+    }
+
     return (
         <main>
             <h1>Sign Up</h1>
-            <p>This is the sign-up page.</p>
+
+            {
+                formErrors.length > 0 &&
+                    <div>
+                        <p>Sign up unsuccessful: </p>
+                        <ul>
+                            {
+                                formErrors.map((errMsg) => {
+                                    return <li key={errMsg}>{errMsg}</li>;
+                                })
+                            }
+                        </ul>
+                    </div>
+            }
+
+            <form onSubmit={ handleFormSubmit }>
+                <label>
+                    First Name:
+                    <input type='text' name='firstName' value={inputValues.firstName} 
+                        onChange={ handleInputChange }>
+                    </input>
+                </label>
+                <br/>
+
+                <label>
+                    Last Name: 
+                    <input type='text' name='lastName' value={inputValues.lastName}
+                        onChange={ handleInputChange }>
+                    </input>
+                </label>
+                <br/>
+
+                <label>
+                    Role: 
+                    <input type='text' name='role' value={inputValues.role}
+                        onChange={ handleInputChange }>
+                    </input>
+                </label>
+                <br/>
+
+                <label>
+                    Username: 
+                    <input type='text' name='username' value={inputValues.username}
+                        onChange={ handleInputChange }>
+                    </input>
+                </label>
+                <br/>
+
+                <label>
+                    Password: 
+                    <input type='text' name='password' value={inputValues.password}
+                        onChange={ handleInputChange }>
+                    </input>
+                </label>
+                <br/>
+
+                <label>
+                    Confirm Password: 
+                    <input type='text' name='confirmPassword' value={inputValues.confirmPassword}
+                        onChange={ handleInputChange }>
+                    </input>
+                </label>
+                <br/>
+
+                <button type='submit'>Sign Up</button>
+            </form>
         </main>
     );
 }
