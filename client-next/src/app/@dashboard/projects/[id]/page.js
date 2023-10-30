3c3dc5efb1e1812e4843bede4b829640859e0402
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-10-30 14:18:45 -0700

    Add single project details page

---
+++ client-next/src/app/@dashboard/projects/[id]/page.js
@@ -0,0 +1,75 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { apiURL } from '../../../../../config.js';
+
+export default function ProjectDetailsPage({ params }) {
+   const [project, setProject] = useState();
+
+   useEffect(() => {
+      async function getProject() {
+         try {
+            const fetchOptions = {
+               method: 'GET',
+               mode: 'cors',
+               credentials: 'include',
+               cache: 'no-store'
+            };
+            const fetchURL = apiURL + '/projects/' + params.id;
+
+            const res = await fetch(fetchURL, fetchOptions);
+            const data = await res.json();
+
+            if (res.ok) {
+               const projectData = data.data;
+               setProject(projectData);
+            } else {
+               const errors = data.errors;
+               console.log(errors);
+            }
+         } catch (err) {
+            console.error(err);
+         }
+      }
+
+      getProject();
+   }, []);
+
+   return (
+      <main>
+         { 
+            project && 
+               <>
+                  <h1>{project.name}</h1>
+                  <p>This is the project detail page.</p>
+
+                  <ul>
+                     <li>Name: {project.name}</li>
+                     <li>Date Created: {project.dateCreated}</li>
+                     <li>Status: {project.status}</li>
+                     <li>Priority: {project.priority}</li>
+                     <li>Lead: {
+                           project.lead.firstName + ' ' +
+                           project.lead.lastName
+                        }
+                     </li>
+                     <li>
+                        Team Members: 
+                        <ul>
+                           {
+                              project.team.map((member) => {
+                                 return (
+                                    <li key={member._id}>
+                                       {member.firstName + ' ' + member.lastName}
+                                    </li>
+                                 );
+                              })
+                           }
+                        </ul>
+                     </li>
+                  </ul>
+               </>
+         }
+      </main>
+   );
+}
