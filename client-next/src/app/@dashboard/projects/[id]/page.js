df4bda721a9814e3ca1dd31fded0c371480e8604
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-11-03 17:39:22 -0700

    Implement project details page and update form

--- client-next/src/app/@dashboard/projects/[id]/page.js
+++ client-next/src/app/@dashboard/projects/[id]/page.js
@@ -5,13 +5,19 @@ import { apiURL } from '../../../../../config.js';
 
 export default function ProjectDetailsPage({ params }) {
    const [project, setProject] = useState();
+   const [memberList, setMemberList] = useState(); 
+   const [memberMap, setMemberMap] = useState();
+   const [inUpdateMode, setInUpdateMode] = useState(false);
+   const [inputValues, setInputValues] = useState();
+   const [formSubmitted, setFormSubmitted] = useState(false);
+   const [formErrors, setFormErrors] = useState([]);
    const [error, setError] = useState();
 
    if (error) {
       throw error;
    }
 
-   useEffect(() => {
+   useEffect(function fetchProjectData() {
       async function getProject() {
          try {
             const fetchOptions = {
@@ -41,39 +47,405 @@ export default function ProjectDetailsPage({ params }) {
       getProject();
    }, []);
 
+   useEffect(function fetchAllMembers() {
+      // if (!inUpdateMode) {
+      //    return;
+      // }
+      
+      async function getMemberList() {
+         try {
+            const fetchOptions = {
+               method: 'GET',
+               mode:'cors',
+               credentials: 'include',
+               cache: 'no-store'
+            };
+            const fetchURL = apiURL + '/members';
+
+            const res = await fetch(fetchURL, fetchOptions);
+            const data = await res.json();
+
+            if (res.ok) {
+               const memberListData = data.data;
+               const memberListMap = new Map();
+               memberListData.forEach(member => {
+                  memberListMap.set(member._id, member);
+               });
+
+               setMemberList(memberListData);
+               setMemberMap(memberListMap);
+            } else {
+               const errors = data.errors;
+               setError(new Error(errors[0]));
+            }
+         } catch (err) {
+            setError(err);
+         }
+      }
+
+      getMemberList();
+  }, []);
+//when to update memberList?
+   useEffect(function submitProjectUpdateForm() {
+      if (!formSubmitted) {
+         return;
+      }
+
+      async function sendFormData() {
+         try {
+            var fetchBody = { ...inputValues };
+            fetchBody.lead = memberList[inputValues.lead]._id;
+            fetchBody.team = inputValues.team.map(member => member._id);
+            delete fetchBody.selectedAddMemberListIndex;
+
+            var fetchOptions = {
+               method: 'POST',
+               headers: { 
+                     'Content-Type': 'application/json',
+               },
+               body: JSON.stringify(fetchBody),
+               mode: 'cors',
+               credentials: 'include',
+               cache: 'no-store'
+            }
+            var fetchURL = apiURL + '/projects';
+
+            var res = await fetch(fetchURL, fetchOptions);
+            var data = await res.json();
+
+            if (res.ok) {
+               setProject(data.data);
+               setInUpdateMode(false);
+            } else {
+               setFormErrors(data.errors);
+            }
+         } catch (err) {
+            setError(err);
+         }
+
+         setFormSubmitted(false);
+      }
+  
+      sendFormData();
+   }, [formSubmitted]);
+
+   function handleFormSubmit(e) {
+      e.preventDefault();
+      setFormSubmitted(true);
+   }
+
+   function handleUpdateModeToggle() {
+      if (!inUpdateMode) {
+         //Before rendering update form:
+
+         // //mark the current lead and team members in memberListCopy
+         // //then update memberList
+         // let memberListCopy = [ ...memberList ];
+         // const targetMembersMap = new Map(project.team.map(member => [ member._id, 'team' ]));
+         // targetMembersMap.set(project.lead._id, 'lead');
+
+         // let markCount = 0;
+
+         // for (let i = 0; i < memberListCopy.length; i++) {
+         //    if (markCount === targetMembersMap.size) {
+         //       break;
+         //    }
+
+         //    let currMember = memberListCopy[i];
+
+         //    if (targetMembersMap.has(currMember._id)) {
+         //       currMember.currAssignedRole = targetMembersMap.get(currMember._id);
+         //       markCount++;
+         //    }
+         // }
+
+         // setMemberList(memberListCopy);
+         // setInputValues({ 
+         //    ...project, 
+         //    lead: memberListCopy.findIndex(member => member._id === project.lead._id),
+         //    selectedAddMemberListIndex: memberListCopy.findIndex(member => !member.currAssignedRole)
+         // });
+         // setFormErrors([]);
+
+         //initialize inputValues
+         const teamList = project.team;
+         const teamMap = new Map();
+         teamList.forEach(member => {
+            teamMap.set(member._id, member);
+         });
+
+         //keep track of which team member has been selected (not yet added)
+         //since 'add' button cannot send info about which member is currently selected
+         //selectedAddMember must be initialized to the first member that is not in team
+         //debugger;
+         let selectedAddMemberId;
+         for (const keyValPair of memberMap) {
+            const memberId = keyValPair[0];
+
+            if (!teamMap.has(memberId)) {
+               selectedAddMemberId = memberId;
+               break;
+            }
+         }
+
+         setInputValues({ 
+            ...project, 
+            team: teamMap,
+            selectedAddMemberId
+         });
+         setFormErrors([]);
+      }
+
+      setInUpdateMode(prev => !prev);
+   }
+
+   function handleInputChange(e) {  
+      var inputElem = e.target;
+
+      if (inputElem.name === 'lead') {
+         // //remove 'currAssignedRole' property from removed lead in memberList
+         // //so that they show up as an option for lead or team
+         // let memberListCopy = [ ...memberList ];
+         // delete memberListCopy[inputValues.lead].currAssignedRole;
+
+         // memberListCopy[inputElem.value].currAssignedRole = 'lead';
+
+         // setMemberList(memberListCopy);
+
+         setInputValues(prevState => {
+            return { ...prevState, [inputElem.name]: memberMap.get(inputElem.value) };
+         });   
+      } else {
+         setInputValues(prevState => {
+            return { ...prevState, [inputElem.name]: inputElem.value };
+         });   
+      }
+   }
+
+   function handleTeamMemberAdd(e) {
+      // let memberListCopy = [ ...memberList ];
+      // const memberToAdd = memberListCopy[prevState.selectedAddMemberListIndex];
+      // memberToAdd.currAssignedRole = 'team';
+
+      // setMemberList(memberListCopy);
+      // setInputValues(prevState => {
+      //    return { ...prevState, team: [ ...prevState.team, memberToAdd ] };
+      // });
+
+      //will adding the selectedMember cause an update to inputValues.selectedAddMemberId?
+      //if not, inputValues.selectedAddMemberId will contain the stale value of previously 
+      //added member, not the member that is currently selected      
+      setInputValues(prevState => {
+         const newMemberId = inputValues.selectedAddMemberId;
+         const newMember = memberMap.get(newMemberId);
+         const updatedTeam = new Map(prevState.team);
+         updatedTeam.set(newMemberId, newMember);
+
+         //team has been updated, so update selectedMemberId
+         let selectedAddMemberId;
+         for (const keyValPair of memberMap) {
+            const memberId = keyValPair[0];
+   
+            if (!updatedTeam.has(memberId)) {
+               selectedAddMemberId = memberId;
+               break;
+            }
+         }
+
+         return { ...prevState, team: updatedTeam, selectedAddMemberId };
+      });
+   }
+
+   function handleTeamMemberRemove(e) {
+      // var indexToRemove = e.target.dataset.teamIndex;
+
+      // //remove 'currAssignedRole' property from removed team member in memberList
+      // //so that they show up as an option for lead or team
+      // let memberListCopy = [ ...memberList ];
+      // var removedMemberInList = memberListCopy.find(member => member._id === inputValues.team[indexToRemove]._id);
+      // delete removedMemberInList.currAssignedRole;
+
+      // setMemberList(memberListCopy);
+      // setInputValues(prevState => {
+      //    return { ...prevState, team: prevState.team.toSpliced(indexToRemove, 1) };
+      // });
+
+      setInputValues(prevState => {
+         const removedMemberId = e.target.dataset.teamMemberId;
+         const updatedTeam = new Map(prevState.team);
+         updatedTeam.delete(removedMemberId);
+
+         //team has been updated, so update selectedMemberId
+         let selectedAddMemberId;
+         for (const keyValPair of memberMap) {
+            const memberId = keyValPair[0];
+   
+            if (!updatedTeam.has(memberId)) {
+               selectedAddMemberId = memberId;
+               break;
+            }
+         }
+
+         return { ...prevState, team: updatedTeam, selectedAddMemberId };
+      });
+   }
+
    return (
       <main>
          { 
-            project && 
+            (project && memberList) && 
                <>
                   <h1>{project.name}</h1>
                   <p>This is the project detail page.</p>
 
-                  <ul>
-                     <li>Name: {project.name}</li>
-                     <li>Date Created: {project.dateCreated}</li>
-                     <li>Status: {project.status}</li>
-                     <li>Priority: {project.priority}</li>
-                     <li>Lead: {
-                           project.lead.firstName + ' ' +
-                           project.lead.lastName
-                        }
-                     </li>
-                     <li>
-                        Team Members: 
-                        <ul>
-                           {
-                              project.team.map((member) => {
-                                 return (
-                                    <li key={member._id}>
-                                       {member.firstName + ' ' + member.lastName}
-                                    </li>
-                                 );
-                              })
-                           }
-                        </ul>
-                     </li>
-                  </ul>
+                  {
+                     inUpdateMode 
+                        ?
+                           <>
+                              {
+                                 formErrors.length > 0 &&
+                                    <div>
+                                       <p>Project update unsuccessful: </p>
+                                       <ul>
+                                             {
+                                                formErrors.map((errMsg) => {
+                                                   return <li key={errMsg}>{errMsg}</li>;
+                                                })
+                                             }
+                                       </ul>
+                                    </div>
+                              }
+   
+                              <form onSubmit={ handleFormSubmit }>
+                                 <label>
+                                    Name:
+                                    <input 
+                                          type='text' name='name' value={inputValues.name} 
+                                          onChange={ handleInputChange }
+                                    >
+                                    </input>
+                                 </label>
+                                 <br/>
+   
+                                 <label>
+                                    Status: 
+                                    <select 
+                                          name='status' value={inputValues.status} 
+                                          onChange={ handleInputChange }
+                                    >
+                                          <option value='open'>Open</option>
+                                          <option value='inProgress'>In Progress</option>
+                                          <option value='complete'>Complete</option>
+                                          <option value='closed'>Closed</option>
+                                    </select>
+                                 </label>
+                                 <br/>
+   
+                                 <label>
+                                    Priority: 
+                                    <select 
+                                          name='priority' value={inputValues.priority} 
+                                          onChange={ handleInputChange }
+                                    >
+                                          <option value='high'>High</option>
+                                          <option value='medium'>Medium</option>
+                                          <option value='low'>Low</option>
+                                    </select>
+                                 </label>
+                                 <br/>
+   
+                                 <label>
+                                    Lead: 
+                                    <select 
+                                          name='lead' value={inputValues.lead._id} 
+                                          onChange={ handleInputChange }
+                                    >
+                                          { memberList && memberList.map((member, index) => {
+                                             return (
+                                                <option value={member._id} key={member._id}>
+                                                   {`${member.firstName} ${member.lastName} (${member.username})`}
+                                                </option>
+                                             );
+                                          }) }
+                                    </select>
+                                 </label>
+                                 <br/>
+   
+                                 Team Members: 
+                                 <ul>
+                                    { Array.from(inputValues.team, ([ memberId, member ]) => {
+                                       return (
+                                          <li key={memberId}>
+                                             {`${member.firstName} ${member.lastName} (${member.username})`}
+                                             <button type='button' data-team-member-id={memberId} onClick={handleTeamMemberRemove}>Remove</button>
+                                          </li>
+                                       );
+                                    }) }
+                                 </ul>
+                                 <label>
+                                    Add Members
+                                    <br/> 
+                                    <select 
+                                          name='selectedAddMemberId' value={inputValues.selectedAddMemberId} 
+                                          onChange={ handleInputChange }
+                                    >
+                                          { memberList && memberList.map((member, index) => {
+                                             if (inputValues.team.has(member._id)) {
+                                                return null;
+                                             }
+
+                                             return (
+                                                <option value={member._id} key={member._id}>
+                                                   {`${member.firstName} ${member.lastName} (${member.username})`}
+                                                </option>
+                                             );
+                                          }) }
+                                    </select>
+                                    <button type='button' onClick={handleTeamMemberAdd}>Add</button>
+                                 </label>
+                                 <br/>
+   
+                                 <br/>
+                                 <button type='submit'>Update</button>
+                                 <button type='button' 
+                                    onClick={handleUpdateModeToggle}
+                                 >Cancel</button>
+                              </form>
+                           </>
+                        :
+                           <>
+                              <ul>
+                                 <li>Name: {project.name}</li>
+                                 <li>Date Created: {project.dateCreated}</li>
+                                 <li>Status: {project.status}</li>
+                                 <li>Priority: {project.priority}</li>
+                                 <li>Lead: {
+                                       project.lead.firstName + ' ' +
+                                       project.lead.lastName
+                                    }
+                                 </li>
+                                 <li>
+                                    Team Members: 
+                                    <ul>
+                                       {
+                                          project.team.map((member) => {
+                                             return (
+                                                <li key={member._id}>
+                                                   {member.firstName + ' ' + member.lastName}
+                                                </li>
+                                             );
+                                          })
+                                       }
+                                    </ul>
+                                 </li>
+                              </ul>
+
+                              <div>
+                                 <button onClick={handleUpdateModeToggle}>Update</button>
+                                 <button>Delete</button>
+                              </div>
+                        </>
+                  }           
                </>
          }
       </main>
