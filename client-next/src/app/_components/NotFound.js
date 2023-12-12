5b0623fba1bf6905db275e676a287d152195336a
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-12-12 10:29:40 -0800

    Add main element to NotFound page

--- client-next/src/app/_components/NotFound.js
+++ client-next/src/app/_components/NotFound.js
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export default function NotFound() {
    return (
-      <>
+      <main>
          <h1>Sorry...</h1>
          <p>{"We couldn't find what you're looking for."}</p>
          <Link href='/'>Return Home</Link>
-      </>
+      </main>
    );
 };
