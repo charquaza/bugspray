f3bd28753209ff8e41927fb74c2452f974de4ba4
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2023-12-13 11:47:03 -0800

    Remove logout button from home page

--- client-next/src/app/@dashboard/page.js
+++ client-next/src/app/@dashboard/page.js
@@ -1,36 +1,9 @@
 'use client'
 
-import { useRouter } from 'next/navigation';
 import { useUserData } from '@/app/_hooks/hooks';
-import { apiURL } from '@/root/config.js';
 
 export default function Home() {
    const user = useUserData();
-
-   const router = useRouter();
-
-   async function handleLogOut() {
-      try {
-         const fetchOptions = {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'include'
-         }
-         const fetchURL = apiURL + '/members/log-out';
-   
-         const res = await fetch(fetchURL, fetchOptions);
-   
-         if (res.ok) {
-            router.refresh();
-         } else {
-            const data = await res.json();
-            const errors = data.errors;
-            throw Error('Logout unsuccessful: ' + errors[0]);
-         }   
-      } catch (err) {
-         throw err;
-      }
-   }
 
    return (
       <main>
@@ -39,7 +12,6 @@ export default function Home() {
                <>
                   <p>Welcome, { user.username }.</p>
                   <p>This is the dashboard.</p>
-                  <button onClick={handleLogOut}>Log Out</button>
                </>   
          }
       </main>
