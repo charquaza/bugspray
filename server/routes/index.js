09887b6e8bd7d8f3b8d1787d78d6ed79d257eba1
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-01-09 10:38:56 -0800

    Add sprint router

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const memberRouter = require('./member');
 const projectRouter = require('./project');
 const taskRouter = require('./task');
+const sprintRouter = require('./sprint');
 
 router.use('/members', memberRouter);
 router.use('/projects', projectRouter);
 router.use('/tasks', taskRouter);
+router.use('/sprints', sprintRouter);
 
 module.exports = router;
