6b3a1a34df0c0f400a48ae610f28ac3b1101c4de
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-01-31 10:19:31 -0800

    Wrap sprintCreateForm in div

--- client-next/src/app/_components/SprintCreateForm.js
+++ client-next/src/app/_components/SprintCreateForm.js
@@ -69,72 +69,76 @@ export default function SprintList({ projectId }) {
    }
 
    return (
-      showCreateForm 
-         ?
-            <>
-               {
-                  formErrors.length > 0 &&
-                     <div>
-                        <p>Sprint creation unsuccessful: </p>
-                        <ul>
-                              {
-                                 formErrors.map((errMsg) => {
-                                    return <li key={errMsg}>{errMsg}</li>;
-                                 })
-                              }
-                        </ul>
-                     </div>
-               }
+      <div>
+         {
+            showCreateForm 
+               ?
+                  <>
+                     {
+                        formErrors.length > 0 &&
+                           <div>
+                              <p>Sprint creation unsuccessful: </p>
+                              <ul>
+                                    {
+                                       formErrors.map((errMsg) => {
+                                          return <li key={errMsg}>{errMsg}</li>;
+                                       })
+                                    }
+                              </ul>
+                           </div>
+                     }
 
-               <form onSubmit={ handleFormSubmit }>
-                  <label>
-                     Name:
-                     <input 
-                        type='text' name='name' value={inputValues.name} 
-                        onChange={ handleInputChange }
-                     >
-                     </input>
-                  </label>
-                  <br/>
+                     <form onSubmit={ handleFormSubmit }>
+                        <label>
+                           Name:
+                           <input 
+                              type='text' name='name' value={inputValues.name} 
+                              onChange={ handleInputChange }
+                           >
+                           </input>
+                        </label>
+                        <br/>
 
-                  <label>
-                     Description:
-                     <input 
-                        type='text' name='description' value={inputValues.description} 
-                        onChange={ handleInputChange }
-                     >
-                     </input>
-                  </label>
-                  <br/>
+                        <label>
+                           Description:
+                           <input 
+                              type='text' name='description' value={inputValues.description} 
+                              onChange={ handleInputChange }
+                           >
+                           </input>
+                        </label>
+                        <br/>
 
-                  <label>
-                     Start Date:
-                     <DatePicker value={inputValues.startDate} 
-                        onChange={ newValue => setInputValues(prevState => {
-                           return { ...prevState, startDate: newValue }
-                        }) } 
-                     /> 
-                  </label>
-                  <br/>
+                        <label>
+                           Start Date:
+                           <DatePicker value={inputValues.startDate} 
+                              onChange={ newValue => setInputValues(prevState => {
+                                 return { ...prevState, startDate: newValue }
+                              }) } 
+                           /> 
+                        </label>
+                        <br/>
 
-                  <label>
-                     End Date:
-                     <DatePicker value={inputValues.endDate}
-                        onChange={ newValue => setInputValues(prevState => {
-                           return { ...prevState, endDate: newValue }
-                        }) } 
-                     /> 
-                  </label>
-                  <br/>
+                        <label>
+                           End Date:
+                           <DatePicker value={inputValues.endDate}
+                              onChange={ newValue => setInputValues(prevState => {
+                                 return { ...prevState, endDate: newValue }
+                              }) } 
+                           /> 
+                        </label>
+                        <br/>
 
-                  <br/>
-                  <button type='submit'>Create</button>
-                  <button type='button' 
-                     onClick={handleCreateToggle}
-                  >Cancel</button>
-               </form>
-            </>
-         :
-            <button onClick={handleCreateToggle}>Create New Sprint</button>
+                        <br/>
+                        <button type='submit'>Create</button>
+                        <button type='button' 
+                           onClick={handleCreateToggle}
+                        >Cancel</button>
+                     </form>
+                  </>
+               :
+                  <button onClick={handleCreateToggle}>Create New Sprint</button>
+         }
+      </div>
    );
 }
