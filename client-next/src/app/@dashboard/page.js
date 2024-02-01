02d351cd4b0b52ac9b11c0bfb790bc2a1abcd4b0
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2024-02-01 12:12:13 -0800

    Add project content to home page

--- client-next/src/app/@dashboard/page.js
+++ client-next/src/app/@dashboard/page.js
@@ -1,19 +1,60 @@
 'use client'
 
-import { useUserData } from '@/app/_hooks/hooks';
+import { useState, useEffect } from 'react';
+import { apiURL } from '@/root/config.js';
+import DashProjectCard from '@/app/_components/DashProjectCard';
 
 export default function Home() {
-   const user = useUserData();
+   const [projectList, setProjectList] = useState();
+   const [error, setError] = useState();
+
+   if (error) {
+      throw error;
+   }
+
+   useEffect(function getProjectList() {
+      async function fetchProjectList() {
+         try {
+            const fetchOptions = {
+               method: 'GET',
+               mode: 'cors',
+               credentials: 'include',
+               cache: 'no-store'
+            };
+            const fetchURL = apiURL + '/projects';
+
+            const res = await fetch(fetchURL, fetchOptions);
+            const data = await res.json();
+
+            if (res.ok) {
+               const projectListData = data.data;   
+               setProjectList(projectListData);
+            } else {
+               const errors = data.errors;
+               setError(new Error(errors[0]));
+            }
+         } catch (err) {
+            setError(err);
+         }
+      }
+
+      fetchProjectList();
+   }, []);
 
    return (
       <main>
          {
-            user && 
+            projectList &&
                <>
-                  <p>Welcome, { user.username }.</p>
-                  <p>This is the dashboard.</p>
+                  <h1>Your Work, in Progress</h1>
+
+                  {
+                     projectList.map(project => {
+                        return <DashProjectCard project={project} key={project._id} />;
+                     })
+                  }
                </>   
          }
       </main>
    );
-}
+};
---
+++ client-next/src/app/_components/DashProjectCard.js
@@ -0,0 +1,76 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+import { apiURL } from '@/root/config.js';
+import TaskList from '@/app/_components/TaskList';
+
+export default function DashProjectCard({ project }) {
+   const [taskList, setTaskList] = useState();
+   const [error, setError] = useState();
+
+   let completedTaskCount = 0; 
+   if (taskList) {
+      taskList.forEach(task => {
+         if (task.status === 'Complete' || task.status === 'Closed') {
+            completedTaskCount++;
+         }
+      });
+   }
+
+   if (error) {
+      throw error;
+   }
+
+   useEffect(function getTaskList() {
+      async function fetchTaskList() {
+         try {
+            const fetchOptions = {
+               method: 'GET',
+               mode: 'cors',
+               credentials: 'include',
+               cache: 'no-store'
+            };
+            const fetchURL = apiURL + '/tasks' + '?projectId=' + project._id
+
+            const res = await fetch(fetchURL, fetchOptions);
+            const data = await res.json();
+
+            if (res.ok) {
+               const taskListData = data.data; 
+               setTaskList(taskListData);
+            } else {
+               const errors = data.errors;
+               setError(new Error(errors[0]));
+            }
+         } catch (err) {
+            setError(err);
+         }
+      }
+
+      fetchTaskList();
+   }, [project._id]);
+
+   return (
+      <article>
+         <h2>{project.name}</h2>
+
+         <p>
+            Task Completion Rate:&nbsp; 
+            {
+               taskList && 
+                  taskList.length > 0 
+                     ?
+                        completedTaskCount + '/' + taskList.length + 
+                        ` (${Math.round(completedTaskCount / taskList.length * 100)}%)`
+                     : 'N/A'
+            }
+         </p>
+
+         <h3>Upcoming Tasks</h3>
+         <TaskList projectId={project._id} initialPageSize={5}
+            selectColumns={['title', 'description', 'status', 'priority', 'sprint']}
+            filterByStatus={['Open', 'In Progress']}
+         />
+      </article>
+   );
+};
