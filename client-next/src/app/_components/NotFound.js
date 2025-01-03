9f854d488a7f661621b8dcf93d59dcd96d5adfdd
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2025-01-03 09:18:54 -0800

    Add spacing before link to home in NotFound page

--- client-next/src/app/_components/NotFound.js
+++ client-next/src/app/_components/NotFound.js
@@ -1,8 +1,9 @@
 import Link from 'next/link';
+import styles from '@/app/_styles/NotFound.module.css';
 
 export default function NotFound() {
    return (
-      <main>
+      <main className={styles['not-found-container']}>
          <h1>Sorry...</h1>
          <p>{"We couldn't find what you're looking for."}</p>
          <Link href='/'>Return Home</Link>
