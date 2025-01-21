f53f345462eb384e12a915463428b338e2730177
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2025-01-20 19:00:31 -0800

    TaskList - Add table styles

--- client-next/src/app/_components/TaskList.js
+++ client-next/src/app/_components/TaskList.js
@@ -3,11 +3,10 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { DateTime } from 'luxon';
-import { 
-   DataGrid, GridToolbarContainer,
-   GridToolbarDensitySelector 
-} from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
+import sortingComparators from '../_utils/sortingComparators';
 import { apiURL } from '@/root/config.js';
+import styles from '@/app/_styles/TaskList.module.css';
 
 export default function TaskList({ 
    projectId, selectColumns, initialPageSize, filterByStatus 
@@ -57,25 +56,43 @@ export default function TaskList({
       fetchTaskList();
    }, [projectId, filterByStatus]);
 
-   function GridToolbar() {
-      return (
-        <GridToolbarContainer>
-          <GridToolbarDensitySelector />
-        </GridToolbarContainer>
-      );
-    }
-
    var dataGridColumns = [
-      { field: 'title', headerName: 'Title', flex: 0.7, minWidth: 90, renderCell: renderTask },
-      { field: 'description', headerName: 'Description', flex: 1, minWidth: 100 },
-      { field: 'project', headerName: 'Project', width: 90, renderCell: renderProject },
-      { field: 'status', headerName: 'Status', width: 90 },
-      { field: 'priority', headerName: 'Priority', width: 100 },
-      { field: 'dateCreated', headerName: 'Date Created', width: 130, minWidth: 80 },
-      { field: 'createdBy', headerName: 'Created By', width: 120, minWidth: 65, renderCell: renderMember },
-      { field: 'sprint', headerName: 'Sprint', width: 60, renderCell: renderSprint },
-      { field: 'assignees', headerName: 'Assignees', width: 180, minWidth: 60, renderCell: renderAssignees },
+      { field: 'title', headerName: 'Title', flex: 0.7, minWidth: 200, 
+         cellClassName: styles['cell'],
+         renderCell: renderTask, valueGetter: (value) => value 
+      },
+      { field: 'description', headerName: 'Description', flex: 1, minWidth: 150,
+         cellClassName: styles['description-cell']
+      },
+      { field: 'project', headerName: 'Project', flex: 0.4, minWidth: 150, 
+         cellClassName: styles['cell'],
+         renderCell: renderProject, valueGetter: (value) => value 
+      },
+      { field: 'status', headerName: 'Status', minWidth: 100,
+         cellClassName: styles['cell'],
+         sortComparator: sortingComparators.status
+      },
+      { field: 'priority', headerName: 'Priority', minWidth: 80,
+         cellClassName: styles['cell'],
+         sortComparator: sortingComparators.priority
+      },
+      { field: 'dateCreated', headerName: 'Date Created', minWidth: 110,
+         cellClassName: styles['cell'],
+         valueFormatter: (value) => DateTime.fromISO(value).toLocaleString(DateTime.DATE_MED)
+      },
+      // { field: 'createdBy', headerName: 'Created By', minWidth: 65, 
+      //    renderCell: renderMember, valueGetter: (value) => value 
+      // },
+      { field: 'sprint', headerName: 'Sprint', flex: 0.1, minWidth: 100, 
+         cellClassName: styles['cell'],
+         renderCell: renderSprint, valueGetter: (value) => value 
+      },
+      { field: 'assignees', headerName: 'Assignees', flex: 0.3, minWidth: 150,
+         cellClassName: styles['assignees-cell'],
+         renderCell: renderAssignees, valueGetter: (value) => value
+      }
    ];
+
    if (selectColumns) {
       dataGridColumns = dataGridColumns.filter(column => {
          return selectColumns.includes(column.field);
@@ -84,12 +101,7 @@ export default function TaskList({
 
    var dataGridRows = (taskList) 
       ?
-         taskList.map(task => {
-            return ({
-               ...task,
-               dateCreated: DateTime.fromISO(task.dateCreated).toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
-            });
-         })
+         taskList.map(task => task)
       : 
          [];
    
@@ -147,24 +159,55 @@ export default function TaskList({
       );
    }
 
+   function getRowHeight(params) {
+      const titleLength = params.model.title.length;
+      const descriptionLength = params.model.description.length;
+      const assigneesLength = params.model.assignees.length;
+
+      const numOfRows = Math.max(
+         Math.ceil( titleLength / 26 ), 
+         Math.ceil( descriptionLength / 40 ), 
+         assigneesLength
+      );
+
+      if (numOfRows <= 1) {
+         return 45;
+      } else if (numOfRows <= 2) {
+         return 65;
+      } else if (numOfRows <= 3) {
+         return 85;
+      } else {
+         return 110;
+      }
+   }
+
    return (
       taskList &&
-         <div>
+         <div className={styles['task-list-ctnr']}>
             <DataGrid
                getRowId={getRowId}
                rows={dataGridRows}
                columns={dataGridColumns}
-               autoHeight
-               getRowHeight={() => 'auto'}
-               slots={{
-                  toolbar: GridToolbar,
-                }}
+               columnHeaderHeight={55}
+               getRowHeight={getRowHeight}
                initialState={{
                   pagination: { paginationModel: { pageSize: initialPageSize || 25 } },
                 }}
                pageSizeOptions={[5,10,25,50,100]}
                checkboxSelection
                localeText={{ noRowsLabel: 'No tasks to display' }}
+               sx={{
+                  '& .MuiDataGrid-columnHeader': {
+                    backgroundColor: 'rgb(222, 244, 230)',
+                    fontSize: '1.2em'
+                  },
+                  '& .MuiDataGrid-row': {
+                     fontSize: '1.1em'
+                  },
+                  '& .MuiDataGrid-footerContainer': {
+                     backgroundColor: 'rgb(229, 246, 235)',
+                  }
+               }}
             />
          </div>
    );
