c22aca875ba36d8c03f8ffdbd03ac3dcfdc35b9e
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2025-01-22 15:20:52 -0800

    Error Pages - Add styles

--- client-next/src/app/@dashboard/error.js
+++ client-next/src/app/@dashboard/error.js
@@ -1,12 +1,17 @@
 'use client'
 
+import styles from '@/app/_styles/errorPage.module.css';
+
 export default function DashboardError({ error, reset }) {
    return (
-      <main>
+      <main className={styles['error-page']}>
          <h1>Oops.</h1>
          <p>Something went wrong...</p>
-         <p>(A possibility: What you seek, may no longer be.)</p>
-         <p>{ 'Error message: ' + error.message }</p>
+         <p className={styles['hint']}>( A possibility: What you seek, may no longer be. )</p>
+         <p>
+            Error message:&nbsp;
+            <span className={styles['error-msg']}>{error.message}</span>
+         </p>
          <button onClick={() => reset()}>Click Here to Try Again</button>
       </main>
    );
--- client-next/src/app/@landing/error.js
+++ client-next/src/app/@landing/error.js
@@ -1,11 +1,16 @@
 'use client'
 
+import styles from '@/app/_styles/errorPage.module.css';
+
 export default function LandingError({ error, reset }) {
    return (
-      <main>
+      <main className={styles['error-page']}>
          <h1>Oops.</h1>
          <p>Something went wrong...</p>
-         <p>{ 'Error message: ' + error.message }</p>
+         <p>
+            Error message:&nbsp;
+            <span className={styles['error-msg']}>{error.message}</span>
+         </p>
          <button onClick={() => reset()}>Click Here to Try Again</button>
       </main>
    );
