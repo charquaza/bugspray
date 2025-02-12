26af0b608b1e53e158e77e109158b801636464d6
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2025-02-12 12:44:11 -0800

    Add method to remove user from Slack channel, and edit Slack user invite method to log count of users added

--- server/services/slackService.js
+++ server/services/slackService.js
@@ -32,6 +32,7 @@ exports.renameSlackChannel = async function renameSlackChannel(channelId, newNam
 exports.archiveSlackChannel = async function archiveSlackChannel(channelId) {
    try {
       const response = await slackClient.conversations.archive({ channel: channelId });
+      
       console.log(`Slack channel ${channelId} has been archived.`);
       return response;
    } catch (error) {
@@ -47,12 +48,30 @@ exports.inviteUsersToChannel = async function inviteUsersToChannel(channelId, us
          users: userIdList.join()
       });
 
-      console.log(`User(s) added to channel ${channelId}`);
+      userIdList.length > 1
+         ? console.log(`${userIdList.length} Users added to channel ${channelId}`)
+         : console.log(`1 User added to channel ${channelId}`);
+
       return response;
    } catch (error) {
       console.error('Error inviting user(s) to Slack channel: ', error);
    }
 }
+
+exports.removeUserFromChannel = async function removeUserFromChannel(channelId, userId) {
+   try {
+      const response = await slackClient.conversations.kick({
+         channel: channelId,
+         user: userId
+      });
+
+      console.log(`User ${userId} removed from channel ${channelId}`);
+      return response;
+   } catch (error) {
+      console.error('Error removing user from Slack channel: ', error);
+   }
+}
+
 
 exports.sendSlackMessage = async function sendSlackMessage(channelId, message) {
    try {
