fe650b09c1b0a4c2e1a87efae1fd3a0dd6c159fd
Jonathan Park <59178632+charquaza@users.noreply.github.com>
2025-03-02 20:22:37 -0800

    Tasks Page - Update task list after new task creation

--- client-next/src/app/@dashboard/tasks/page.js
+++ client-next/src/app/@dashboard/tasks/page.js
@@ -1,12 +1,25 @@
+'use client'
+
+import { useState } from 'react';
+import { useUserData } from '@/app/_hooks/hooks';
 import TaskCreateForm from '@/app/_components/TaskCreateForm';
 import TaskList from '@/app/_components/TaskList';
 
 export default function TasksPage() {
+   const user = useUserData();
+   const [ updateTaskList, setUpdateTaskList ] = useState(false);
+
    return (
       <main>
          <h1>Tasks</h1>
-         <TaskCreateForm />
-         <TaskList />
+         
+         {(user && user.privilege === 'admin') &&
+            <TaskCreateForm setUpdateTaskList={setUpdateTaskList} />
+         }
+         <TaskList 
+            updateTaskList={updateTaskList} 
+            setUpdateTaskList={setUpdateTaskList}
+         />
       </main>
    );
 };
